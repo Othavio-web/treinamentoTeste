e46bdede02f36bb1513ef383a9474c85deedc282
Othavio-web <66527646+Othavio-web@users.noreply.github.com>
2023-03-20 09:24:53 -0300

    New exemples of colections

--- collection/Exemplo.java
+++ collection/Exemplo.java
@@ -1,12 +1,16 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Exemplo {
     public static void main(String[] args) {
         manipulaLista();
         manipularQueue();
+        manipularMap();
     }
     public static void manipulaLista() {
         ArrayList<String> animais = new ArrayList<String>();
@@ -31,5 +35,21 @@ public class Exemplo {
             System.out.println("quem saiu foi o: "+lista.removeFirst());
         }
         System.out.println("a minha filça possui: "+lista.size());
+    }
+    public static void manipularMap() {
+        HashMap<String, String> dados = new HashMap<String, String>();
+        
+        dados.put("Nome", "Renato");
+        dados.put("Sobrenome", "Chitolina");
+        dados.put("Idade", "62");
+        dados.put("CPF", "000.000.000-00");
+        
+        System.out.println(dados.get("CPF"));
+        
+        Set<Entry<String, String>> dadosIteraveis = dados.entrySet();
+        
+        for (Entry<String, String> entry : dadosIteraveis) {
+            System.out.println("chade" +entry.getKey()+" - valor:"+entry.getValue());
+        }
     }
 }
